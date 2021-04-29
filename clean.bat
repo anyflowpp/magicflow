eb126cf555a858d44ccdbe4a4c34530b1c2054f1
mashichuan <981880785@qq.com>
2021-04-29 17:45:40 +0800

    跨平台适配

--- clean.bat
+++ clean.bat
@@ -1,4 +1,5 @@
 pushd %~dp0
-call "D:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
-devenv build\threadflow.sln /clean
+pushd build
+del /q /r /f *.obj
+popd
 popd
